cacaf1c50688ac1250766a85b92d87999ce41fb2
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-25 22:39:13 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -12,57 +12,75 @@ const inputEl = document.querySelector('#search-box');
 const countryListEL = document.querySelector('.country-list');
 const countryInfoDiv = document.querySelector('.country-info');
 
-//додаємо обробніка подій на інпут
+//додаємо обробника подій на інпут
 inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 //опрацьовуємо отримане значення з інпуту
 function onSearch(event) {
-  event.preventDefault();
-  const inputCountryName = inputEl.value.trim();
-  fetchCountries(inputCountryName)
-    .then(({ data: countries }) => {
-      countriesData(countries);
-      console.log(countries);
-    })
-    .catch(error => {
-      if (inputCountryName !== '') {
-        showNoMatchNotification();
-      }
-    });
+  let inputCountryName = event.target.value.trim();
+  fetchCountries(inputCountryName).then(countries => {
+    console.log(countries);
+    if (countries.length > 10) {
+      clearAll();
+      NeedSpecificNameNotification();
+    }
+    if (countries.length > 1 && countries.length <= 10) {
+      clearAll();
+      countriesListLayOut(countries);
+    }
+    if (countries.length === 1) {
+      clearAll();
+      countriesCard(countries);
+    }
+    if (countries.length === 0) {
+      clearAll();
+      showNoMatchNotification();
+    }
+  });
 }
-function countriesData(countries) {
-  if (countries.length > 10) {
-    clearAll();
-
-    NeedSpecificNameNotification();
-  } else if (countries.length > 1 && countries.length <= 10) {
-    clearAll();
-    return countries
-      .map(
-        country => `<li class = "country">
-        <img src = "${country.flags.svg}"/>
-        <p class = "country-name">${country.name.official}</p>
-        </li>`
-      )
-      .join('');
-    countryListEL.innerHTML;
-  } else clearAll();
-  return countries
+//--------------------------------------
+function countriesListLayOut(countries) {
+  let markup = countries
     .map(
       country =>
         `<div class = "country-card">
-    <img  class = "country-flag"
+    <img  class = "country-flag" width="80"
     src = "${country.flags.svg}"
-     alt = "${country.name.official} flag"
-    <p class = "country-name">${country.name.official}</p>
-    <p class= "capital-name">${country.capital}</p>
-    <p class = "population">${country.population}</p>
-    <p class = "language">${Object.values(languages)}</p>
+     alt = "${country.name.official} flag" />
+    <p class = "country-name">Country: ${country.name.official}</p>
+    <p class= "capital-name">Capital: ${country.capital}</p>
+    <p class = "population">Population: ${country.population}</p>
+    <p class = "language">Language: ${Object.values(country.languages)}</p>
     </div>`
     )
     .join('');
-  countryInfoDiv.innerHTML;
+  countryListEL.innerHTML = markup;
 }
+//------------------------------
+function countriesCard(countries) {
+  let countryInfo = countries
+    .map(
+      country =>
+        `<li class = "country">
+     <img  class = "country-flag" width="80"
+    src = "${country.flags.svg}"
+     alt = "${country.name.official} flag" />
+      <p class = "country-name">Country: ${country.name.official}</p>
+      </li>`
+    )
+    .join('');
+  countryInfoDiv.innerHTML = countryInfo;
+}
+//     .map(
+//       country => `<li class = "country">
+//       <img src = "${country.flags.svg}"/>
+//       <p class = "country-name">${country.name.official}</p>
+//       </li>`
+//     )
+//     .join('');
+//   countryListEL.innerHTML;
+// } else clearAll();
+// return countries
 
 function showNoMatchNotification() {
   Notiflix.Notify.failure('Oops, there is no country with that name');
